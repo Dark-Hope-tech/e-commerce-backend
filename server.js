aada1ff818e05bb94b6def14143ddb3e14619995
Tabish Malik <76650437+Dark-Hope-tech@users.noreply.github.com>
2024-07-11 02:38:39 +0530

    fixed

--- server.js
+++ server.js
@@ -24,21 +24,21 @@ app.use(express.json());
 //   })
 // );
 app.use(cors());
-// app.use('/',(req,res)=>{
-//   res.send('server is running');
-// });
+app.use('/api',(req,res)=>{
+  res.send('server is running');
+});
 app.use('/api/foods', foodRouter);
 app.use('/api/users', userRouter);
 app.use('/api/orders', orderRouter);
 app.use('/api/upload', uploadRouter);
 
-const publicFolder = path.join(__dirname, 'public');
-app.use(express.static(publicFolder));
+// const publicFolder = path.join(__dirname, 'public');
+// app.use(express.static(publicFolder));
 
-app.get('*', (req, res) => {
-  const indexFilePath = path.join(publicFolder, 'index.html');
-  res.sendFile(indexFilePath);
-});
+// app.get('*', (req, res) => {
+//   const indexFilePath = path.join(publicFolder, 'index.html');
+//   res.sendFile(indexFilePath);
+// });
 
 const PORT = 3001;
 app.listen(PORT, () => {
